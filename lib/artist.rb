6cc9e824e88a00af49a1d2f5822d752ebbc6ee6f
Carolyn Huynh & Amy Vaillancourt-Sals <student@epicodus.com>
2014-08-04 10:21:02 -0700

    add methods for listing all artists and albums

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,18 @@
 class Artist
   attr_reader :artist, :album
+    @@all_artists = []
+    @@all_albums = []
 
   def initialize(release)
     @artist = release[:artist]
     @album = release[:album]
+  end
+
+  def all_artists
+    @@all_artists
+  end
+
+  def all_albums
+    @@all_albums
   end
 end
--- spec/cd_spec.rb
+++ spec/cd_spec.rb
@@ -17,4 +17,22 @@ describe Artist do
     new_cd = Artist.new({:album => 'songs from the big chair'})
     expect(new_cd.album).to eq 'songs from the big chair'
   end
+
+  it 'starts with an empty array' do
+    new_cd = Artist.new({:artist => 'tears for fears'})
+    expect(new_cd.all_artists).to eq []
+  end
+
+  it 'starts with an empty array' do
+    new_cd = Artist.new({:album => 'songs from the big chair'})
+    expect(new_cd.all_albums).to eq []
+  end
+
+  # it 'lists out all the artists' do
+  #   new_cd = Artist.new({:artist => 'tears for fears'})
+  #   new_cd1 = Artist.new({:artist => 'peter frampton'})
+  #   new_cd2 = Artist.new({:artist => 'tom petty'})
+  #   expect(new_cd.all_artists).to eq ['tears for fears', 'peter frampton', 'tom petty']
+  # end
+
 end
